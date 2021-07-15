0b5b7c80110c8a23e2ddd17c00dc347e82531838
Fajarullah <frullah12@gmail.com>
2021-07-15 17:46:31 +0700

    load data pada onCreate sqlite

--- lib/services/database_handler.dart
+++ lib/services/database_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:aplikasi_organisasi_mobile/model/anggota.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -20,6 +22,48 @@ class DatabaseHandler {
             status TINYINT NOT NULL DEFAULT 0
           )""",
         );
+
+        List<Anggota> listAnggota = [
+          Anggota(
+              kode: "OG-2107001",
+              nama: "Ahmad sanusi",
+              tanggalRegistrasi: "13-07-2021",
+              alamat: "Serang",
+              telepon: "0812xxxxx",
+              aktif: true),
+          Anggota(
+              kode: "OG-2107002",
+              nama: "Awaludin",
+              tanggalRegistrasi: "04-08-2019",
+              alamat: "Jakarta",
+              telepon: "08125xxxx",
+              aktif: true),
+          Anggota(
+              kode: "OG-2107003",
+              nama: "Rani Amelia",
+              tanggalRegistrasi: "20-11-2015",
+              alamat: "Bandung",
+              telepon: "0877xxxxx",
+              aktif: false),
+          Anggota(
+              kode: "OG-2107004",
+              nama: "Reni",
+              tanggalRegistrasi: "11-11-2018",
+              alamat: "Jakarta",
+              telepon: "0815xxxxx",
+              aktif: false),
+          Anggota(
+              kode: "OG-2107005",
+              nama: "Cici",
+              tanggalRegistrasi: "12-06-2011",
+              alamat: "Palembang",
+              telepon: "08122xxxx",
+              aktif: true)
+        ];
+
+        for (var anggota in listAnggota) {
+          await database.insert('anggota', anggota.toMap());
+        }
       },
       version: 1,
     );
